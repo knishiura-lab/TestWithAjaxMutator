c9dbe047bb7b37c3b114e120e3230565ede66c55
kazuki <k-nishiura@nii.ac.jp>
2014-01-20 20:27:31 +0900

    94% coverage

--- src/test/jp/gr/java_conf/ajax_mutator/example/MediaWikiBookCreatorTest.java
+++ src/test/jp/gr/java_conf/ajax_mutator/example/MediaWikiBookCreatorTest.java
@@ -1,11 +1,19 @@
 package jp.gr.java_conf.ajax_mutator.example;
 
+import com.google.common.base.Predicate;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 public class MediaWikiBookCreatorTest extends TestBase {
     public MediaWikiBookCreatorTest() {
@@ -13,10 +21,62 @@ public class MediaWikiBookCreatorTest extends TestBase {
     }
 
     @Test
-    public void testSomething() {
+    public void testSomething() throws InterruptedException {
         openUrl("http://localhost/ex/mediawiki/index.php/Main_Page");
         handleLogin();
         handleStart();
+
+        int count = getAddedBookCount();
+        waitUntil(ExpectedConditions.presenceOfElementLocated(By.id("coll-add_article")));
+        findElement(By.id("coll-add_article")).click();
+        waitUntil(ExpectedConditions.presenceOfElementLocated(By.id("coll-remove_article")));
+        final int count1 = getAddedBookCount();
+        assertEquals(count + 1, count1);
+
+        Actions action = new Actions(getDriver());
+        action.moveToElement(findElement(By.xpath("//*[@id=\"mw-content-text\"]/ul[2]/li[1]/a[1]")));
+        action.pause(1000);
+        action.moveToElement(findElement(By.id("collectionpopup")));
+        action.perform();
+        assertFalse(findElement(By.id("collectionpopup")).getAttribute("style").contains("none"));
+
+        action = new Actions(getDriver());
+        action.moveToElement(findElement(By.xpath("//*[@id=\"mw-content-text\"]/ul[2]/li[1]/a[1]")));
+        action.pause(1000);
+        action.moveToElement(findElement(By.id("collectionpopup")));
+        action.pause(1000);
+        action.moveByOffset(100,100);
+        action.pause(1000);
+        assertFalse(findElement(By.id("collectionpopup")).getAttribute("style").contains("none"));
+
+        action = new Actions(getDriver());
+        action.moveToElement(findElement(By.xpath("//*[@id=\"mw-content-text\"]/ul[2]/li[1]/a[1]")));
+        action.pause(1000);
+        action.moveToElement(findElement(By.id("collectionpopup")));
+        action.perform();
+        findElement(By.id("collectionpopup")).findElement(By.xpath("a")).click();
+        waitUntil(new Predicate<WebDriver>() {
+            @Override
+            public boolean apply(WebDriver webDriver) {
+                int count2 = getAddedBookCount();
+                return count1 + 1 == count2;
+            }
+        });
+        int count2 = getAddedBookCount();
+        assertEquals(count1 + 1, count2);
+
+    }
+
+    private int getAddedBookCount(){
+        int count = -1;
+        String showBook = findElement(By.xpath("//*[@id=\"coll-book_creator_box\"]/a[2]")).getText();
+        Pattern p = Pattern.compile("\\d");
+        Matcher m = p.matcher(showBook);
+        if(m.find()) {
+            count = new Integer(m.group());
+        }
+
+        return count;
     }
 
     /**
