f542838cfb5e729964cc34f3610976c8a85e30da
kazuki <k-nishiura@nii.ac.jp>
2014-01-20 15:19:59 +0900

    line coverage 97%, branch 83%, function 97%

--- src/test/jp/gr/java_conf/ajax_mutator/example/MemoryGameTest.java
+++ src/test/jp/gr/java_conf/ajax_mutator/example/MemoryGameTest.java
@@ -24,12 +24,65 @@ public class MemoryGameTest extends TestBase {
     }
 
     @Test
-    public void testSomething() {
-        // Please implement here
+    public void testWhole() throws InterruptedException {
         openUrl("http://localhost/ex/p3.monkeyaround.biz/index.php");
-        // Please implement here
+        waitAndClick("div.ui-dialog:nth-child(22) > div:nth-child(3) > div:nth-child(1) > button:nth-child(1)");
+        waitAndClick("div.ui-dialog:nth-child(22) > div:nth-child(3) > div:nth-child(1) > button:nth-child(1)");
+        waitAndClick("#start_stop");
+        Thread.sleep(15000);
+//        waitAndClick("#timer");S
+//        waitAndClick("#start_stop");
+//        waitAndClick("#cheat");
+        for (int i = 0; i < 20; i++) {
+        	for (int j = 0; j < 20; j++) {
+                waitAndClick("#box_" + i);
+                waitAndClick("#box_" + j);
+        	}
+        }
+        Thread.sleep(5000);
+        waitAndClick("div.ui-dialog:nth-child(40) > div:nth-child(3) > div:nth-child(1) > button:nth-child(1)");
     }
 
+    @Test
+    public void testCheet() throws InterruptedException {
+        openUrl("http://localhost/ex/p3.monkeyaround.biz/index.php");
+        waitAndClick("div.ui-dialog:nth-child(22) > div:nth-child(3) > div:nth-child(1) > button:nth-child(1)");
+        waitAndClick("div.ui-dialog:nth-child(22) > div:nth-child(3) > div:nth-child(1) > button:nth-child(1)");
+        waitAndClick("#start_stop");
+        Thread.sleep(15000);
+        waitAndClick("#cheat");
+        Thread.sleep(5000);
+    }
+
+    @Test
+    public void testStop() throws InterruptedException {
+        openUrl("http://localhost/ex/p3.monkeyaround.biz/index.php");
+        waitAndClick("div.ui-dialog:nth-child(22) > div:nth-child(3) > div:nth-child(1) > button:nth-child(1)");
+        waitAndClick("div.ui-dialog:nth-child(22) > div:nth-child(3) > div:nth-child(1) > button:nth-child(1)");
+        waitAndClick("#start_stop");
+        Thread.sleep(15000);
+        waitAndClick("#start_stop");
+    }
+    
+    @Test
+    public void testUserKeyword() throws InterruptedException {
+        openUrl("http://localhost/ex/p3.monkeyaround.biz/index.php");
+        waitAndClick("div.ui-dialog:nth-child(22) > div:nth-child(3) > div:nth-child(1) > button:nth-child(1)");
+        waitAndClick("div.ui-dialog:nth-child(22) > div:nth-child(3) > div:nth-child(1) > button:nth-child(1)");
+    	By keywordIn = By.cssSelector("#imageKeywords");
+        waitUntil(ExpectedConditions.presenceOfElementLocated(keywordIn));
+        findElement(keywordIn).sendKeys("dog");
+        waitAndClick("#loadUserImages");
+        waitAndClick("#loadUserImages");
+        waitAndClick("#start_stop");
+        Thread.sleep(2000);
+    }
+    private void waitAndClick(String sel) {
+    	By button = By.cssSelector(sel);
+        waitUntil(ExpectedConditions.visibilityOfElementLocated(button));
+        findElement(button).click();
+    }
+    
     @Override
     protected String getOriginalFileRoot() {
         return "/var/www/ex/p3.monkeyaround.biz";
