b9f2785a9915c9984e5a65f98fa17e8d18d2e07f
kazuki <k-nishiura@nii.ac.jp>
2014-01-20 15:33:08 +0900

    done

--- src/test/jp/gr/java_conf/ajax_mutator/example/MediaWikiBookCreatorTest.java
+++ src/test/jp/gr/java_conf/ajax_mutator/example/MediaWikiBookCreatorTest.java
@@ -1,24 +1,86 @@
 package jp.gr.java_conf.ajax_mutator.example;
 
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class MediaWikiBookCreatorTest extends TestBase {
     public MediaWikiBookCreatorTest() {
-        super(/* calc coverage */true, /* run in background */false);
+        super(/* calc coverage */false, /* run in background */true);
+    }
+    
+    @Before
+    public void setUp() {
+    	openUrl("http://localhost/ex/mediawiki/index.php/Main_Page");
+        handleLogin();
+        handleStart();
+        waitUntil(ExpectedConditions.presenceOfElementLocated(By.className("collection-creatorbox")));
     }
 
     @Test
-    public void testSomething() {
-        openUrl("http://localhost/ex/mediawiki/index.php/Main_Page");
-        handleLogin();
-        handleStart();
+    public void toggleArticle_shouldToggleArticleLink() {        
+    	findElement(By.id("coll-add_article")).click();
+        waitUntil(ExpectedConditions.presenceOfElementLocated(By.id("coll-remove_article")));
+        findElement(By.id("coll-remove_article")).click();
+        waitUntil(ExpectedConditions.presenceOfElementLocated(By.id("coll-add_article")));
     }
-
+    
+    @Test
+    public void toggleLinkedPage_shouldToggleLinkedPage() throws Exception {
+    	Actions actions = new Actions(getDriver());
+        actions.moveToElement(findElement(By.xpath("//*[@id='mw-content-text']/ul[2]/li[1]/a"))).build().perform();
+        
+        WebElement popupElement = findElement(By.id("collectionpopup"));
+        waitUntil(ExpectedConditions.visibilityOf(popupElement));
+        assertTrue(popupElement.findElement(By.xpath("//*[@id='collectionpopup']/a/img")).getAttribute("src")
+        		.endsWith("mediawiki/extensions/Collection/images/silk-add.png"));
+        
+        actions.moveToElement(popupElement.findElement(By.tagName("a"))).build().perform();
+        popupElement.findElement(By.tagName("a")).click();        
+        
+        actions.moveToElement(findElement(By.xpath("//*[@id='mw-content-text']/ul[1]/li[1]/a"))).build().perform();
+        sleep(2);
+        
+        actions.moveToElement(findElement(By.xpath("//*[@id='mw-content-text']/ul[2]/li[1]/a"))).build().perform();
+        waitUntil(ExpectedConditions.visibilityOf(popupElement));        
+        assertTrue(popupElement.findElement(By.xpath("//*[@id='collectionpopup']/a/img")).getAttribute("src")
+        		.endsWith("/mediawiki/extensions/Collection/images/silk-remove.png"));
+        
+        actions.moveToElement(popupElement.findElement(By.tagName("a"))).build().perform();
+        popupElement.findElement(By.tagName("a")).click();
+        
+        actions.moveToElement(popupElement.findElement(By.tagName("a"))).build().perform();
+        popupElement.findElement(By.tagName("a")).click();        
+        
+        actions.moveToElement(findElement(By.xpath("//*[@id='mw-content-text']/ul[1]/li[1]/a"))).build().perform();
+        sleep(2);
+        
+        actions.moveToElement(findElement(By.xpath("//*[@id='mw-content-text']/ul[2]/li[1]/a"))).build().perform();
+        waitUntil(ExpectedConditions.visibilityOf(popupElement));        
+        assertTrue(popupElement.findElement(By.xpath("//*[@id='collectionpopup']/a/img")).getAttribute("src")
+        		.endsWith("/mediawiki/extensions/Collection/images/silk-add.png"));
+        
+    }
+    
+    private void sleep(int sec) {
+    	try{
+    		Thread.sleep(sec * 1000);
+    	}
+    	catch(Exception e) {
+    		
+    	}
+    }
+    
     /**
      * Start book creator
      */
